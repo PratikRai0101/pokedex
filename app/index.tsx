2962715a3efc82aa9923a9d9c6aa7c300bd1b963
Pratik Rai <raipratik0101@gmail.com>
2026-01-04 23:03:27 +0530

    interface created

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function Index() {
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]); 
   
   useEffect(() => {
       //fetch pokemon from api
@@ -15,21 +22,21 @@ export default function Index() {
       );
       const data = await response.json();
 
-      console.log(data);
+      setPokemons(data.results);
     } catch (e) {
       console.log(e);
     }
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Pokedex</Text>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>
+            {pokemon.name} 
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
