6d6f71ee6be9eae7ea610455b7598bdaeb3debdb
Pratik Rai <raipratik0101@gmail.com>
2026-01-05 20:38:02 +0530

    detents added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout() {
     <Stack.Screen name="index" options={{title: "Home"}}/>
     <Stack.Screen name="details" options={{title: "Details", 
       headerBackButtonDisplayMode: "minimal",
-      presentation: "modal",
+      presentation: "formSheet",
+      sheetGrabberVisible: true,
+      sheetAllowedDetents: [0.3 , 0.7, 1],
       headerShown: true
       }}/>
   </Stack>
