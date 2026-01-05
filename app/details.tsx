431b6b5266ed08899bf7350486517b6e237a9343
Pratik Rai <raipratik0101@gmail.com>
2026-01-05 20:30:17 +0530

    details tab fetch req added

--- app/details.tsx
+++ app/details.tsx
@@ -6,6 +6,23 @@ export default function Details() {
     const params = useLocalSearchParams();
 
     console.log(params);
+
+    useEffect(() => {
+        //fetch pokemon details from api
+        fetchPokemonDetailsByName(params.name as string);
+    }, []);
+
+    async function fetchPokemonDetailsByName(name: string) {
+        try {
+            const response = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${name}`
+            );
+            const data = await response.json();
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
   return (
     <ScrollView
       contentContainerStyle={{ 
